68476f3db9c2e6c6d23069dd4bcae96a5001a872
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2022-11-30 17:50:08 +0100

    Add calculator and test to verify, that test coverage works

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(1, 2);
+        assertEquals(3, result);
+    }
+}
